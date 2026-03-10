a879227833c57535ef2b10e3d0dfab523dff09f6
Iñaki Aliende <121037002+ialiendeucm@users.noreply.github.com>
2026-03-10 09:31:30 +0100

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Script from "next/script";
 
 export const metadata = {
   title: "Iñaki Aliende, PhD",
@@ -14,14 +15,21 @@ export default function RootLayout({
     <html lang="en">
       <body className="bg-white text-slate-900 antialiased">
         {children}
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-BQ3S1S9QP2"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'G-BQ3S1S9QP2');
-</script>
+
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-BQ3S1S9QP2"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            window.gtag = gtag;
+            gtag('js', new Date());
+            gtag('config', 'G-BQ3S1S9QP2');
+          `}
+        </Script>
       </body>
     </html>
   );
